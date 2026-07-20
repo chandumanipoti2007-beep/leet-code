d936b5f96293eaccdd1ed04f21b3608dd9b0144d
chandumanipoti2007-beep <chandumanipoti2007@gmail.com>
2026-07-20 15:01:56 +0530

    Time: 5 ms (80.69%) | Memory: 45.9 MB (72.56%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int rev=0;
+        int b=x;
+        if (x<0)return false;
+        while(x!=0){
+            int digit=x%10;
+            rev=rev*10+digit;
+            x=x/10;
+        }
+        return (rev==b);
+        
+    }
+}
